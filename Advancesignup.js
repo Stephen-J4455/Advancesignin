ce98c53301a122741fe06f08cfa3c4cfd577d812
Stephen-J4455 <164698827+Stephen-J4455@users.noreply.github.com>
2024-05-26 15:40:35 +0000

    Add files via upload

---
+++ Advancesignup.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCG7fRw21apfTA36HKl5LbVWiHI_8zk12A",
+    authDomain: "signup-bfcf8.firebaseapp.com",
+    projectId: "signup-bfcf8",
+    storageBucket: "signup-bfcf8.appspot.com",
+    messagingSenderId: "862509936181",
+    appId: "1:862509936181:web:6a16490f27a97bac7bcb7c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
